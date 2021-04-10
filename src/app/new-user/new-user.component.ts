861dbb8b78b34ea240c517d01645c5e5b6f8c46e
himanshuarora1994@gmail.com <himanshu.arora1994gmail.com>
2021-04-10 21:02:29 +0530

    asaAS

--- src/app/new-user/new-user.component.ts
+++ src/app/new-user/new-user.component.ts
@@ -21,7 +21,7 @@ export class NewUserComponent implements OnInit {
               private router: Router) { }
 
   ngOnInit() {
-    console.log("master1");
+    console.log("masternwq1");
     this.route.params.subscribe(
       (params: Params) => {
         console.log('paramms', params);
